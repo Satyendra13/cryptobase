df8d141bec347d12c1a25daf3b633b53056889de
Satyendra Kumar <satyaku001@gmail.com>
2022-06-14 11:23:41 +0530

    first commit

---
+++ src/config/firebaseConfig.js
@@ -0,0 +1,11 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyAM7_qOowjQgOcq0XIYu2ifieyGRQoIuhk",
+  authDomain: "cryptocurrency-dashboard-a95a3.firebaseapp.com",
+  projectId: "cryptocurrency-dashboard-a95a3",
+  storageBucket: "cryptocurrency-dashboard-a95a3.appspot.com",
+  messagingSenderId: "155621574577",
+  appId: "1:155621574577:web:1b49611ab81f5b1bc4df90",
+  measurementId: "G-QT10RHD3KQ"
+};
+
+export default firebaseConfig;
